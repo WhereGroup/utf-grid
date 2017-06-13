748425cf5bcf8cfb9e7c77fc4dd82ca1449b2ae5
ransomware <mohamed.tahrioui@wheregroup.com>
2017-06-13 16:02:41 +0200

    Add utfgrid widget placeholder

---
+++ Resources/public/mapbender.element.utfgrid.js
@@ -0,0 +1,21 @@
+(function($) {
+
+$.widget("mapbender.mbUTFGrid", {
+    options: {},
+
+    _create: function() {
+        // TODO: make use of:
+        // Mapbender.configuration.elements
+    },
+    _setup: function(id) {},
+
+    destroy: function() {},
+
+    _bindToLayer: function(mqLayer) {},
+
+    _destroy: function() {}
+
+});
+
+})(jQuery);
+
